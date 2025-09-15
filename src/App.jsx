5f8493eeae205c221f3a127302da7a47e8e3a3df
Rajesh Kumar Str <vikoshiya.rajeshkumarstr@gmail.com>
2025-09-15 13:30:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,24 @@ import UnderMaintenance from './pages/UnderMaintenance.jsx'
 
 function App() {
   return (
-    // <div className="font-sans bg-gray-50">
-    //   <ScrollToTop />
-    //   <Navigation />
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/blog" element={<Blog />} />
-    //     <Route path="/contact" element={<Contact />} />
-    //     <Route path="/privacy" element={<Privacy />} />
-    //     <Route path="/terms" element={<Terms />} />
-    //     <Route path="/web-development" element={<WebDevelopment />} />
-    //     <Route path="/quality-assurance" element={<QualityAssurance />} />
-    //     <Route path="/blog/:id" element={<BlogPost />} />
-    //     <Route path="/ecommerce" element={<Ecommerce />} />
-    //     <Route path="/under-maintenance" element={<UnderMaintenance />} />
-    //   </Routes>
-    // </div>
-    <UnderMaintenance />
+    <div className="font-sans bg-gray-50">
+      <ScrollToTop />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/web-development" element={<WebDevelopment />} />
+        <Route path="/quality-assurance" element={<QualityAssurance />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+        <Route path="/ecommerce" element={<Ecommerce />} />
+        <Route path="/under-maintenance" element={<UnderMaintenance />} />
+      </Routes>
+    </div>
+    // <UnderMaintenance />
   )
 }
 
